598e3737213f64a2d5791f96b3cd25f8a20555b4
jwiegner <jwiegner@ethz.ch>
2026-06-02 16:39:36 +0200

    For server

--- main_run_mean_variance.py
+++ main_run_mean_variance.py
@@ -17,7 +17,7 @@ from zen_garden.plugins.mean_variance_optimization.plugin import config as plugi
 on_server = True
 example_dataset = False
 base = 10e-9
-weights = [x * base for x in [25,30,35,40,45,50,55,60,65,70,75,80,85,90,95,100]]
+weights = [x * base for x in [25, 50, 75, 100]]
 
 
 # PATHS
@@ -55,79 +55,79 @@ with open("./config.json", "w") as f:
     json.dump(config, f, indent=4)
 
 run(config="./config.json", dataset=dataset, folder_output=result_folder)
-#
-# include_var_for = {
-#     "capex": ["technology_capex"],
-#     # "capex_opex": ["capex", "opex"],
-#     # "capex_opex_import_export": ["capex", "opex", "import", "export"],
-#     # "all": ["technology_capex", "technology_opex", "import", "export", "demand_shedding"]
-# }
-#
-#
-# for variance_inclusion in include_var_for.keys():
-#
-#     for i, weight in enumerate(weights):
-#         if i == 0:
-#             result_folder = f"./outputs_{time_str}/{variance_inclusion}/lambda_{str(weight)}"
-#
-#             if not os.path.exists(result_folder):
-#                 os.makedirs(result_folder)
-#
-#             with open("./config.json") as f:
-#                 config = json.load(f)
-#
-#             config["plugins"]["mean_variance_optimization"] = {}
-#             config["plugins"]["mean_variance_optimization"]["weighting_factor"] = weight
-#             config["plugins"]["mean_variance_optimization"]["include_variances_for"] = include_var_for[variance_inclusion]
-#             config["solver"]["solver_options"]["LogFile"] =  f"{result_folder}/solver.log"
-#
-#             with open("./config_quadratic.json", "w") as f:
-#                 json.dump(config, f, indent=4)
-#
-#             optimization_setup = run(
-#                 config="./config_quadratic.json",
-#                 dataset=dataset,
-#                 folder_output=result_folder,
-#             )
-#
-#         else:
-#             result_folder = f"./outputs_{time_str}/{variance_inclusion}/lambda_{str(weight)}"
-#
-#             if not os.path.exists(result_folder):
-#                 os.makedirs(result_folder)
-#
-#             plugin_config["weighting_factor"] = weight
-#             optimization_setup.solver.solver_options["LogFile"] =  f"{result_folder}/solver.log"
-#             optimization_setup.solver.solver_dir =  result_folder
-#             optimization_setup.analysis.folder_output =  result_folder
-#
-#             EventPublisher.trigger(Event.after_model_construction, optimization_setup=optimization_setup)
-#
-#             if optimization_setup.solver.use_scaling:
-#                 optimization_setup.scaling.run_scaling()
-#             elif (
-#                     optimization_setup.solver.analyze_numerics
-#                     or optimization_setup.solver.run_diagnostics
-#             ):
-#                 optimization_setup.scaling.analyze_numerics()
-#             # SOLVE THE OPTIMIZATION PROBLEM
-#             optimization_setup.solve()
-#
-#             if optimization_setup.solver.use_scaling:
-#                 optimization_setup.scaling.re_scale()
-#
-#             # EVALUATE RESULTS
-#             scenarios = {'': {}}
-#             subfolder = Path(".")
-#             model_name = 'Crystal_Ball'
-#             scenario_name = None
-#             param_map = None
-#             # write results
-#             Postprocess(
-#                 optimization_setup,
-#                 scenarios=scenarios,
-#                 subfolder=subfolder,
-#                 model_name=model_name,
-#                 scenario_name=scenario_name,
-#                 param_map=param_map,
-#             )
+
+include_var_for = {
+    "capex": ["technology_capex"],
+    # "capex_opex": ["capex", "opex"],
+    # "capex_opex_import_export": ["capex", "opex", "import", "export"],
+    # "all": ["technology_capex", "technology_opex", "import", "export", "demand_shedding"]
+}
+
+
+for variance_inclusion in include_var_for.keys():
+
+    for i, weight in enumerate(weights):
+        if i == 0:
+            result_folder = f"./outputs_{time_str}/{variance_inclusion}/lambda_{str(weight)}"
+
+            if not os.path.exists(result_folder):
+                os.makedirs(result_folder)
+
+            with open("./config.json") as f:
+                config = json.load(f)
+
+            config["plugins"]["mean_variance_optimization"] = {}
+            config["plugins"]["mean_variance_optimization"]["weighting_factor"] = weight
+            config["plugins"]["mean_variance_optimization"]["include_variances_for"] = include_var_for[variance_inclusion]
+            config["solver"]["solver_options"]["LogFile"] =  f"{result_folder}/solver.log"
+
+            with open("./config_quadratic.json", "w") as f:
+                json.dump(config, f, indent=4)
+
+            optimization_setup = run(
+                config="./config_quadratic.json",
+                dataset=dataset,
+                folder_output=result_folder,
+            )
+
+        else:
+            result_folder = f"./outputs_{time_str}/{variance_inclusion}/lambda_{str(weight)}"
+
+            if not os.path.exists(result_folder):
+                os.makedirs(result_folder)
+
+            plugin_config["weighting_factor"] = weight
+            optimization_setup.solver.solver_options["LogFile"] =  f"{result_folder}/solver.log"
+            optimization_setup.solver.solver_dir =  result_folder
+            optimization_setup.analysis.folder_output =  result_folder
+
+            EventPublisher.trigger(Event.after_model_construction, optimization_setup=optimization_setup)
+
+            if optimization_setup.solver.use_scaling:
+                optimization_setup.scaling.run_scaling()
+            elif (
+                    optimization_setup.solver.analyze_numerics
+                    or optimization_setup.solver.run_diagnostics
+            ):
+                optimization_setup.scaling.analyze_numerics()
+            # SOLVE THE OPTIMIZATION PROBLEM
+            optimization_setup.solve()
+
+            if optimization_setup.solver.use_scaling:
+                optimization_setup.scaling.re_scale()
+
+            # EVALUATE RESULTS
+            scenarios = {'': {}}
+            subfolder = Path(".")
+            model_name = 'Crystal_Ball'
+            scenario_name = None
+            param_map = None
+            # write results
+            Postprocess(
+                optimization_setup,
+                scenarios=scenarios,
+                subfolder=subfolder,
+                model_name=model_name,
+                scenario_name=scenario_name,
+                param_map=param_map,
+            )
